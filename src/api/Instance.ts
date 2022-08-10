1e072d4387c0a82f023c9a53e66980d81cebde05
Sárffy Gergő <sarffygeri@gmail.com>
2022-08-10 23:49:00 +0200

    fixed v2 public .env variables

--- src/api/Instance.ts
+++ src/api/Instance.ts
@@ -2,8 +2,8 @@ import axios from "axios";
 
 const instance = axios.create({
   baseURL:
-    process.env.STAGE == "LIVE"
-      ? process.env.API_BASE_URL
+    process.env.NEXT_PUBLIC_STAGE == "LIVE"
+      ? process.env.NEXT_PUBLIC_API_BASE_URL
       : "http://localhost:3000/api/v1",
   timeout: 10000,
 });
