4604d8651a5c210bf32d703c299be0f239aa6b38
Sárffy Gergő <sarffygeri@gmail.com>
2022-08-10 23:32:49 +0200

    fix

--- src/api/Login.ts
+++ src/api/Login.ts
@@ -2,6 +2,9 @@ import { Instance } from "api";
 import { LoginProps } from "types";
 
 export default async function login({ email, password }: LoginProps) {
+  console.log(process.env.STAGE);
+  console.log(process.env.API_BASE_URL);
+
   return Instance.post("/login", { email, password })
     .then((res) => res.data)
     .catch((err) => err.response.data);
