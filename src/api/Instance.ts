9a05eec06a3bb761257cd48a856a4c2343b4d6c1
Sárffy Gergő <sarffygeri@gmail.com>
2022-08-10 23:17:18 +0200

    test new API URL

--- src/api/Instance.ts
+++ src/api/Instance.ts
@@ -1,11 +1,10 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL:
-    process.env.STAGE == "prod"
+  baseURL: process.env.API_BASE_URL,
+  /*process.env.STAGE == "prod"
       ? process.env.API_BASE_URL
-      : "http://localhost:3000/api/v1",
-  timeout: 10000,
+      : "http://localhost:3000/api/v1"*/ timeout: 10000,
 });
 
 export default instance;
