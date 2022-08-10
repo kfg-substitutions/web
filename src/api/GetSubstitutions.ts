17de290f5f6f15449cd55b5a3df918a75540c590
Sárffy Gergő <sarffygeri@gmail.com>
2022-08-10 23:44:23 +0200

    fixed public next environment files

--- src/api/GetSubstitutions.ts
+++ src/api/GetSubstitutions.ts
@@ -3,5 +3,5 @@ import { Instance } from "api";
 export default async function getSubstitutions() {
   return Instance.get("/get_substitutions")
     .then((res) => res.data)
-    .catch((err) => err.response.data);
+    .catch((err) => err.response?.data);
 }
--- src/api/Login.ts
+++ src/api/Login.ts
@@ -2,10 +2,7 @@ import { Instance } from "api";
 import { LoginProps } from "types";
 
 export default async function login({ email, password }: LoginProps) {
-  console.log(process.env.STAGE);
-  console.log(process.env.API_BASE_URL);
-
   return Instance.post("/login", { email, password })
     .then((res) => res.data)
-    .catch((err) => err.response.data);
+    .catch((err) => err.response?.data);
 }
