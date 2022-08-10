3187cdf7871df4b23dd1fec4b7bf09ade6a7ea2e
Sárffy Gergő <sarffygeri@gmail.com>
2022-08-10 23:28:30 +0200

    fixing api url location

--- src/api/Instance.ts
+++ src/api/Instance.ts
@@ -1,10 +1,11 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: process.env.API_BASE_URL,
-  /*process.env.STAGE == "prod"
+  baseURL:
+    process.env.STAGE == "LIVE"
       ? process.env.API_BASE_URL
-      : "http://localhost:3000/api/v1"*/ timeout: 10000,
+      : "http://localhost:3000/api/v1",
+  timeout: 10000,
 });
 
 export default instance;
