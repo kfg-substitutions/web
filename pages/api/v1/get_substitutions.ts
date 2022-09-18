5f1833ddba62eda65b0c5aef6c11dc76e41075ea
Sárffy Gergő <sarffygeri@gmail.com>
2022-09-18 22:08:00 +0200

    wont work

--- pages/api/v1/get_substitutions.ts
+++ pages/api/v1/get_substitutions.ts
@@ -14,6 +14,21 @@ export default withSentry(function handler(
   req: NextApiRequest,
   res: NextApiResponse<any>
 ) {
+  return res.status(200).json({
+    success: true,
+    info: [
+      process.env.NEXT_PUBLIC_STAGE,
+      process.env.AWS_REGION,
+      process.env.AWS_ACCESS_KEY_ID,
+      process.env.AWS_SECRET_ACCESS_KEY,
+      process.env.AWS_DYNAMODB_TABLE,
+      process.env.ADMIN_LOGIN_EMAIL,
+      process.env.ADMIN_LOGIN_PASSWORD,
+      process.env.ADMIN_LOGIN_TOKEN,
+      process.env.SENTRY_IGNORE_API_RESOLUTION_ERROR,
+    ],
+  });
+
   return substitutions.get((allSubstitutions: APICallResponse) => {
     if (!allSubstitutions.success)
       return res.status(200).json({
