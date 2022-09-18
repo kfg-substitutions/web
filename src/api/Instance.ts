0cc88130e2731b0f54372e2abd66f1830284ba01
Sárffy Gergő <sarffygeri@gmail.com>
2022-09-18 23:05:29 +0200

    nem értem mi van

--- src/api/Instance.ts
+++ src/api/Instance.ts
@@ -1,10 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL:
-    process.env.APP_NEXT_PUBLIC_APP_STAGE == "LIVE"
-      ? "https://" + process.env.VERCEL_URL + "/api/v1"
-      : "http://localhost:3000/api/v1",
+  baseURL: "https://web-tau-liart.vercel.app/api/v1",
   timeout: 10000,
 });
 
