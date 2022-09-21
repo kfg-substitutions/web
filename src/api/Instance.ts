dd372c34e70342e62bfe1e8aa49c189452747155
Sárffy Gergő <sarffygeri@gmail.com>
2022-09-21 19:54:28 +0200

    logging vercel env data

--- src/api/Instance.ts
+++ src/api/Instance.ts
@@ -1,7 +1,26 @@
 import axios from "axios";
 
+// if we fucking hard code this dogshit
+// it will work
+
+// BUT like this it wont
+// and i also hate niggers
+
+console.error(
+  process.env.APP_NEXT_PUBLIC_APP_STAGE,
+  process.env.VERCEL_URL,
+  "https://" + process.env.VERCEL_URL + "/api/v1",
+  "http://localhost:3000/api/v1",
+  process.env.APP_NEXT_PUBLIC_APP_STAGE == "LIVE"
+    ? "https://" + process.env.VERCEL_URL + "/api/v1"
+    : "http://localhost:3000/api/v1"
+);
+
 const instance = axios.create({
-  baseURL: "https://web-tau-liart.vercel.app/api/v1",
+  baseURL:
+    process.env.APP_NEXT_PUBLIC_APP_STAGE == "LIVE"
+      ? "https://" + process.env.VERCEL_URL + "/api/v1"
+      : "http://localhost:3000/api/v1",
   timeout: 10000,
 });
 
